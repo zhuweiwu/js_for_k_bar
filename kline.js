33884fb251778ef3993d80fb78c8e9853c4c7e94
zhuwei <zhuwei@blackpx.com>
2021-11-02 15:33:00 -0500

    <chore> does not finish data operation

--- kline.js
+++ kline.js
@@ -36,10 +36,103 @@ function buildBar(tickdata, barperiod)
 
     let length = tickdata.length;
     let isfirst = true;
+    let time;
+    let ask;
+    let bid;
+
+    //process first tick
+    let firstline = tickdata[0];
+
+    let firststrs = tickdata[0].split(',');
+        
+    if(firststrs.length == 4)
+    {
+      time = new Date(firststrs[0] + " " + firststrs[1]);
+      bid = Number(firststrs[2]);
+      ask = Number(firststrs[3]);
+    }
+    else if(firststrs.length == 5)
+    {
+      time = new Date(firststrs[0] + " " + firststrs[1] + "." + firststrs[2]);
+      bid = Number(firststrs[3]);
+      ask = Number(firststrs[4]);
+    }
+
+    let firstTime = time.getDate()
+
+
     for(var i=0; i<length; i++)
     {
+        let strs = tickdata[i].split(',');
         
+        if(strs.length == 4)
+        {
+          time = new Date(strs[0] + " " + strs[1]);
+          bid = Number(strs[2]);
+          ask = Number(strs[3]);
+        }
+        else if(strs.length == 5)
+        {
+          time = new Date(strs[0] + " " + strs[1] + "." + strs[2]);
+          bid = Number(strs[3]);
+          ask = Number(strs[4]);
+        }
+        else
+        {
+          continue;
+        }
+
     }
     
+}
 
+class Bar
+{
+  constructor()
+  {
+
+  }
+
+  constructor(time, bid, ask)
+  {
+    this.bidOpen = bid;
+    this.bidHigh = bid;
+    this.bidLow = bid;
+    this.bidClose = bid;
+    this.askOpen = ask;
+    this.askHigh = ask;
+    this.askLow = ask;
+    this.askClose = ask;
+    this.time = time;
+  }
+
+  upate(bid, ask)
+  {
+    //update high
+    this.askHigh = this.askHigh > ask ? askHigh : ask;
+    this.bidHigh = this.bidHigh > bid ? bidHigh : bid;
+
+    //update low 
+    this.askLow = this.askLow < ask ? askLow : ask;
+    this.bidLow = this.bidLow < bid ? bidLow : bid;
+
+    //update close
+    this.askClose = ask;
+    this.bidClose = bid;
+  }
+
+  clone()
+  {
+    let newBar = new Bar();
+    newBar.askOpen = bar.askOpen;
+    newBar.askHigh = bar.askHigh;
+    newBar.askLow = bar.askLow;
+    newBar.askClose = bar.askClose;
+    newBar.bidOpen = bar.bidOpen;
+    newBar.bidHigh = bar.bidHigh;
+    newBar.bidLow = bar.bidLow;
+    newBar.bidClose = bar.bidClose;
+    newBar.time = bar.time;
+    return newBar;
+  }
 }
